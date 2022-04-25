bab821221d98ee24a7426aecd7ef3806cf38c949
humutayran <h.umutayran@gmail.com>
2022-04-25 22:10:21 +0300

    sayfaya göre arama hemen hemen eklendi

---
+++ src/deneme/FilmPenceresi.java
@@ -0,0 +1,18 @@
+package deneme;
+
+import org.json.JSONArray;
+
+public class FilmPenceresi {
+    private JSONArray movies;
+    private int movieIndex;
+
+    public FilmPenceresi(JSONArray movies, int movieIndex) {
+        this.movies = movies;
+        this.movieIndex = movieIndex;
+    }
+
+    public void openFrame() {
+
+    }
+}
+
